fdd739f4f000b0b70058f4c6515661504634b23b
williamneto <william.g.neto@gmail.com>
2013-02-13 15:31:44 -0200

    #9 Iniciando front-end da página inicial

--- intelectual/urls.py
+++ intelectual/urls.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, include, url
 
+from .views import HomePageView
+
 urlpatterns = patterns('',
+	url(r'^$', HomePageView.as_view()),
 	url(r'^auth/', include('intelectual.accounts.urls')),
 	url(r'^admin/', include('intelectual.admin.urls')),
 )
--- intelectual/views.py
+++ intelectual/views.py
@@ -3,8 +3,8 @@
 from django.views.generic import TemplateView
 
 from intelectual.videos.models import Video
-
 from intelectual.categorias.models import Categoria
 
 class HomePageView(TemplateView):
+	template_name = "home_page.html"
 	
