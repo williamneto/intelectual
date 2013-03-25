249c486358ec54678fa6992bde4f7578c4e25d18
williamneto <william.g.neto@gmail.com>
2013-03-25 17:12:27 -0300

    _query com categoria e termo

--- intelectual/home/views.py
+++ intelectual/home/views.py
@@ -59,7 +59,7 @@ class HomePageView(TemplateView):
         
         if categoria and term:
             """
-            Pesquisa por um vidoe em uma categoria
+            Pesquisa por um videos em uma categoria
             """
             categoria = Categoria.objects.get(pk=categoria)
             videos = Video.objects.filter(categoria=categoria)
@@ -67,12 +67,12 @@ class HomePageView(TemplateView):
             videos = videos.filter(nome__icontains=term)
 
             videos_list = []
-            for video in videos_list:
+            for video in videos:
             	videos_list.append(video.to_json)
 
             return HttpResponse(simplejson.dumps(videos_list), content_type="application/json")
 
-  		raise Http404
+        raise Http404
 
 	def get(self, *args, **kwargs):
 		cmd = self.request.GET.get('cmd')
