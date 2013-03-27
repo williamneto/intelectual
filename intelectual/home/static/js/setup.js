6ef2cb408c93e64cdc25dd70c70dc13f8150c5e8
williamneto <william.g.neto@gmail.com>
2013-03-27 00:42:05 -0300

    melhorias no js

--- intelectual/home/static/js/setup.js
+++ intelectual/home/static/js/setup.js
@@ -1,11 +1,36 @@
 function Setup(){
+    _instance = this;
     
-    var params = {
-        "cmd": "get_initial_wall"
+    WALL_LIST = [];
+    WALL_INDEX = 0;
+    
+    this.setWallList = function(wallList){
+        WALL_LIST = wallList;
+    }    
+    this.getWallList = function(){
+        return WALL_LIST;
     }
-    $.getJSON(".", params, function(data){
+    
+    this.setWallIndex = function(i){
+        WALL_INDEX = i;
+    }
+    this.getWallIndex = function(i){
+        return WALL_INDEX;
+    }
+    
+    this.show = function(){
+        var wall = _instance.getWallList();
+        var i = _instance.getWallIndex();        
+        var video = wall[i];
+        
         video_inicial_container = $("#video_inicial_container");
-        video_iframe = $(data[0].iframe);
-        video_inicial_container.html(video_iframe);        
-    });
+        video_iframe = $(video.iframe);
+        video_inicial_container.html(video_iframe);
+    }
+    this.next = function(){
+        var i = _instance.getWallIndex();
+        i += 1;
+        _instance.setWallIndex(i);
+        
+    }
 }
